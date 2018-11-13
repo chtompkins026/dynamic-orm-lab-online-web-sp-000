fd348736e64e569f48aadf1708dd18f02bd12c2e
Christopher Tompkins <chtompkins026@gmail.com>
2018-11-13 20:38:42 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,7 +2,8 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  attr_accessor :id, :name, 
+  attr_accessor :id, :name
+  
   def initialize(options={})  
     options.each do |property, value| 
       self.send("#{property}=",value)
